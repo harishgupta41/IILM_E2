5aadee8516140b482275f5a2f07151b588bc2709
harish <guptaharish872@gmail.com>
2024-02-08 23:09:59 +0530

    added functionality to login

--- hackathon/index.py
+++ hackathon/index.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, make_response
 import mysql.connector
-# import uuid
+from user_methods import hash_sha_256
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -15,38 +15,60 @@ app=Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home_before_login.html',title="home")
+    return render_template('home.html',title="home")
 
-@app.route('/home_user')
-def home_user():
-    return render_template('home.html',title='home')
+# @app.route('/home_user')
+# def home_user():
+#     return render_template('home.html',title='home')
 
-@app.route('/login')
-def login():
-    return render_template('login.html',title="login")
+# @app.route('/login')
+# def login():
+#     return render_template('login.html',title="login")
 
 @app.route('/user_login',methods=['GET','POST'])
 def user_login():
     if request.method=="POST":
         username=request.form['username']
         password=request.form['passwd']
-        # cursor.execute('select * from ')
-        return redirect('/')
+        cursor.execute('select * from students where username="{0}" and password="{1}"'.format(username,hash_sha_256(password)))
+        data=cursor.fetchall()
+        if(data):
+            resp=make_response(redirect('/'))
+            resp.set_cookie('user',username)
+            return resp
+        else:
+            return redirect('/')
+            
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html',title="signup")
+# @app.route('/signup')
+# def signup():
+#     return render_template('signup.html',title="signup")
 
 @app.route('/user_signup',methods=['GET','POST'])
 def user_signup():
     if request.method=="POST":
+        print(request.form[0])
         fullname=request.form['fname']
         email=request.form['email']
         phone=request.form['phone']
         username=request.form['username']
-        # userid=uuid.uuid4().hex
+        password=request.form['password']
+        cnfm_password=request.form['cnfmpassword']
+        if password!=cnfm_password:
+            return redirect('/')
+        else:
+            cursor.execute('insert into students values ("{0}","{1}","{2}","{3}","{4}")'.format(username,fullname,email,hash_sha_256(password),phone))
+            mydb.commit()
+            return render_template('access.html',title="registration-success!")
         
-        return redirect('/')
+        
+        # return redirect('/')
+
+@app.route('/logout')
+def logout():
+    response = make_response(render_template('logout.html',title='logging-out'))
+    response.set_cookie('user', '', max_age=0)
+    return response
 
 
 if __name__=="__main__":
---
+++ hackathon/user_methods.py
@@ -0,0 +1,22 @@
+import random, hashlib
+# from twilio import Client
+
+# def getOTP():
+#     return random.randint(100000,999999)
+
+# def getOTPapi(num):
+#     account_sid='AC69378198459d32c41da6a06c14efdcdd'
+#     auth_token='5fbe686a599b2b2a0b5037fe8a3787fa'
+#     client = Client(account_sid,auth_token)
+#     otp=getOTP()
+#     msg_body='Your OTP is '+str(otp)
+#     message={}
+
+
+def hash_sha_256(data):
+    data=bytes(data,'utf-8')
+    sha=hashlib.sha256()
+    sha.update(data)
+    return sha.hexdigest()
+
+print(hash_sha_256("qwerty"))
